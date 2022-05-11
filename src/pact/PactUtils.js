77668c769afb7435caed17d80225f7b07dec4a4a
mohannadwahmad17 <mohannadwahmad97@gmail.com>
2022-05-10 22:03:19 -0500

    fixed annoying dependency issue

--- src/pact/PactUtils.js
+++ src/pact/PactUtils.js
@@ -8,12 +8,19 @@ async function executePactContract(pactContextObject, pactCmd) {
     return contractOutput;
 }
 
+//Retrieve the Pact command to get NFTs for given owner
 function getPactCommandForNftsByOwner(account) {
     return `(free.kadcars-nft-collection.get-kadcars-for-owner "${account}")`;
 }
 
+//Retrieve the Pact command to for NFT using given ID
 function getPactCommandForNftByNftId(nftId) {
     return `(free.kadcars-nft-collection.get-kadcar-for-nft-id "${nftId}")`;
+}
+
+//Retrieve the Pact command to Mint new NFT ID
+function getPactCommandForMintingNftId(account, amount, collection) {
+    return `(free.kadcars-nft-collection)`;
 }
 
 export {
