e024759e9483fe050d926f0b06b0292e30eaf09c
mohannadwahmad17 <mohannadwahmad97@gmail.com>
2022-05-19 00:33:15 -0500

    changing broswer extension hook to use pact context

--- src/hooks/BrowserExtensionHooks.js
+++ src/hooks/BrowserExtensionHooks.js
@@ -1,13 +1,16 @@
-import { useState, useEffect } from "react";
+import { useContext, useState, useEffect } from "react";
+import { PactContext } from "../pact/PactContextProvider";
 
 function useCheckForXWalletExtension() {
+    const { isXwallet, setIsXwallet } = useContext(PactContext);
     const [hasExtension, setHasExtension] = useState(null);
 
     useEffect(() => {
-        setHasExtension(window?.kadena?.isKadena === true);
+        // setHasExtension(window?.kadena?.isKadena === true);
+        setIsXwallet(window?.kadena?.isKadena === true);
     });
 
-    return hasExtension;
+    return isXwallet;
 }
 
 export {
