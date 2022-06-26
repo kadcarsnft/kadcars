2094e9cee4577554df5f99a7ca0be0c02fde97bb
mohannadwahmad17 <mohannadwahmad97@gmail.com>
2022-06-26 17:58:42 -0500

    commented footer

--- src/layouts/LayoutDefault.js
+++ src/layouts/LayoutDefault.js
@@ -1,17 +1,26 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Header from '../components/layout/Header';
 import Footer from '../components/layout/Footer';
 import { MainBackgroundCard, MainBody } from './StyledLayoutComponents';
+import { KadcarGameContext } from '../components/kadcarcomponents/KadcarGameContextProvider';
 
-const LayoutDefault = ({ children }) => (
-  <MainBody>
-    <MainBackgroundCard>
-      <main className="site-content" style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-        <Header navPosition="right" className="reveal-from-bottom" style={{ marginTop: '15px' }} />
-        {children}
-      </main>
-    </MainBackgroundCard>
-  </MainBody>
-);
+const LayoutDefault = ({ children }) => {
+  const kadcarGameContext = useContext(KadcarGameContext);
+
+  return (
+    <MainBody>
+      <MainBackgroundCard>
+        <main className="site-content" style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+          <Header navPosition="right" className="reveal-from-bottom" style={{ paddingTop: '15px' }} />
+          { children }
+          {/* {
+            kadcarGameContext.currentScreen === null &&
+            <Footer topDivider />
+          } */}
+        </main>
+      </MainBackgroundCard>
+    </MainBody>
+  )
+};
 
 export default LayoutDefault;  
