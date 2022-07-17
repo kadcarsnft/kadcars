cdd80a1445c99a43dcdef4c33b327ea3dce56aa3
mohannadwahmad17 <mohannadwahmad97@gmail.com>
2022-07-17 01:24:11 -0500

    added firebase setup

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
-// import ReactDOM from 'react-dom';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from "react-router-dom";
 import { createBrowserHistory } from "history";
+import { initializeApp } from 'firebase/app';
+// import ReactDOM from 'react-dom';
 
 import App from './App';
 import * as serviceWorker from './serviceWorker';
@@ -10,17 +11,24 @@ import * as serviceWorker from './serviceWorker';
 //import './App.css';
 import './assets/scss/style.scss';
 
+//declare firebase config object
+const firebaseConfig = {
+  apiKey: "AIzaSyDSNdC4PlyGukXL4ko7CpYJdAKsd2thfTo",
+  authDomain: "kadcarsnft-28bea.firebaseapp.com",
+  projectId: "kadcarsnft-28bea",
+  storageBucket: "kadcarsnft-28bea.appspot.com",
+  messagingSenderId: "830803935946",
+  appId: "1:830803935946:web:82a45ac6d2aae04c6f6a33"
+};
+
+//Initialize firebase app
+initializeApp(firebaseConfig);
+
 const history = createBrowserHistory();
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 
-// ReactDOM.render(
-//   <BrowserRouter history={history}>
-//     <App />
-//   </BrowserRouter>,
-//   document.getElementById('root')
-// );
 root.render(
   <BrowserRouter history={history}>
     <App />
