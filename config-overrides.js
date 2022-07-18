187625632263c1a54e2ef3839c1cdb19852b136c
mohannadwahmad17 <mohannadwahmad97@gmail.com>
2022-07-17 22:09:10 -0500

    changes to webpack config file

--- config-overrides.js
+++ config-overrides.js
@@ -1,13 +1,28 @@
-module.exports = function override (config, env) {
-    console.log('override')
-    let loaders = config.resolve
-    loaders.fallback = {
-        "fs": false,
-        "tls": false,
-        "net": false,
+const webpack = require('webpack');
+
+module.exports = function override(config) {
+    console.log(config)
+    const fallback = config.resolve.fallback || {};
+    Object.assign(fallback, {
+        "crypto": require.resolve("crypto-browserify"),
+        "stream": require.resolve("stream-browserify"),
+        "assert": require.resolve("assert"),
         "http": require.resolve("stream-http"),
         "https": require.resolve("https-browserify"),
-    }
-    
-    return config
+        "path": require.resolve("path-browserify"),
+        "os": require.resolve("os-browserify"),
+        "url": require.resolve("url"),
+        "fs": false,
+        "net": false,
+        "tls": false,
+    })
+    config.resolve.fallback = fallback;
+    config.plugins = (config.plugins || []).concat([
+        new webpack.ProvidePlugin({
+            process: 'process/browser',
+            Buffer: ['buffer', 'Buffer']
+        })
+    ])
+
+    return config;
 }
